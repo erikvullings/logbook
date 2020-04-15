529c06413e826eb2fab82cf47171864b3887da3e
Erik Vullings <erik.vullings@gmail.com>
2020-04-15 17:15:37 +0200

    Added README for GUI

---
+++ packages/gui/README.md
@@ -0,0 +1,7 @@
+# Installation
+
+When installing the application, add an `.env` in this folder, indicating where the GUI can find the server to retrieve its settings.
+
+```bash
+SERVER=http://localhost:3030/api
+```
--- packages/gui/src/models/app-state.ts
+++ packages/gui/src/models/app-state.ts
@@ -1,6 +1,6 @@
 /** During development, use this URL to access the server. */
-const apiService = `http://localhost:3030/api`;
-// const apiService = process.env.SERVER || window.location.origin; // `http://localhost:${process.env.LOKI_PORT}/`;
+// const apiService = `http://localhost:3030/api`;
+const apiService = process.env.SERVER || window.location.origin;
 
 /** Application state */
 export const AppState = {
